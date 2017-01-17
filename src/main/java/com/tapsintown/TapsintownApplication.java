e901a638993461ecf3e966e9a5e282d2531404d3
Anthony Fortney <afortney86@gmail.com>
2017-01-17 14:06:07 -0600

    Initial commit

---
+++ src/main/java/com/tapsintown/TapsintownApplication.java
@@ -0,0 +1,12 @@
+package com.tapsintown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapsintownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapsintownApplication.class, args);
+	}
+}
