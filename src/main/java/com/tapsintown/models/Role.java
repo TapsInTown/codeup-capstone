22581b8481936e1592ed1c1edfd01caf9783dc79
Anthony Fortney <afortney86@gmail.com>
2017-01-17 16:12:15 -0600

    Completed role model

---
+++ src/main/java/com/tapsintown/models/Role.java
@@ -0,0 +1,36 @@
+package com.tapsintown.models;
+
+import javax.persistence.*;
+
+/**
+ * Created by anthonyfortney on 1/17/17.
+ */
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String role;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
