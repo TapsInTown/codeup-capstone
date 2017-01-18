7516163ca3d35107633bd1413eb47d65f0563a56
Anthony Fortney <afortney86@gmail.com>
2017-01-18 16:32:40 -0600

    added ProfileController

--- src/main/java/com/tapsintown/controllers/LocationController.java
+++ src/main/java/com/tapsintown/controllers/LocationController.java
@@ -1,11 +1,14 @@
 package com.tapsintown.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by anthonyfortney on 1/18/17.
  */
 
 @Controller
+@RequestMapping("/locations")
 public class LocationController {
+
 }
