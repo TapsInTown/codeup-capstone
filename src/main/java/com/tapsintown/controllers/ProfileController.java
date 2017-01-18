371ea648b2e3b2e1360e1b57561440a7b6c399de
Anthony Fortney <afortney86@gmail.com>
2017-01-18 16:10:17 -0600

    added ProfileController

---
+++ src/main/java/com/tapsintown/controllers/ProfileController.java
@@ -0,0 +1,17 @@
+package com.tapsintown.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by anthonyfortney on 1/18/17.
+ */
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile")
+    public String showProfilePage(){
+        return "/profile";
+    }
+}
