8fd15d791775f60e43c617318ce30a3c4c734352
Anthony Fortney <afortney86@gmail.com>
2017-01-18 15:13:14 -0600

    Added controllers Events, Location, and users

---
+++ src/main/java/com/tapsintown/controllers/LocationController.java
@@ -0,0 +1,11 @@
+package com.tapsintown.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by anthonyfortney on 1/18/17.
+ */
+
+@Controller
+public class LocationController {
+}
