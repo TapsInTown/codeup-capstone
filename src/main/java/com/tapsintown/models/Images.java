b067ff6b1e25211edbc63e0f0bf2551906e152b0
Anthony Fortney <afortney86@gmail.com>
2017-01-18 07:49:41 -0600

    images model

--- src/main/java/com/tapsintown/models/Images.java
+++ src/main/java/com/tapsintown/models/Images.java
@@ -1,7 +1,38 @@
 package com.tapsintown.models;
 
+import javax.persistence.*;
+
 /**
  * Created by anthonyfortney on 1/17/17.
  */
+
+@Entity
+@Table(name = "images")
 public class Images {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+
 }
