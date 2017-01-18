35bc47b4e07c59b6e1a11f78e39464fd1853b4d5
Anthony Fortney <afortney86@gmail.com>
2017-01-18 15:09:39 -0600

    added Authentication code

---
+++ src/main/java/com/tapsintown/controllers/AuthenticationController.java
@@ -0,0 +1,15 @@
+package com.tapsintown.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by anthonyfortney on 1/18/17.
+ */
+@Controller
+public class AuthenticationController {
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+}
