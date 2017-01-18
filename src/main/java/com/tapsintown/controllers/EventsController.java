42a571cbd83e80233fc29b4b8ae99784bc58460e
Anthony Fortney <afortney86@gmail.com>
2017-01-18 17:01:18 -0600

    Create controllers for events and locations. Made corresponding html pages.

--- src/main/java/com/tapsintown/controllers/EventsController.java
+++ src/main/java/com/tapsintown/controllers/EventsController.java
@@ -1,11 +1,36 @@
 package com.tapsintown.controllers;
 
+import com.tapsintown.interfaces.Events;
+import com.tapsintown.models.Event;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by anthonyfortney on 1/18/17.
  */
 
 @Controller
+@RequestMapping("/events")
 public class EventsController {
+
+    @Autowired
+    private Events eventsDao;
+
+    @GetMapping("/create")
+    public String showCreateEventForm(Model m){
+        m.addAttribute("event", new Event());
+        return "/events";
+    }
+
+    @PostMapping("/create")
+    public String createNewEvent(@ModelAttribute Event eventCreated){
+        eventsDao.save(eventCreated);
+        return "/events";
+    }
+
 }
--- src/main/java/com/tapsintown/controllers/LocationController.java
+++ src/main/java/com/tapsintown/controllers/LocationController.java
@@ -1,6 +1,13 @@
 package com.tapsintown.controllers;
 
+import com.tapsintown.interfaces.EventLocations;
+import com.tapsintown.models.EventLocation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,5 +17,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/locations")
 public class LocationController {
+
+    @Autowired
+    private EventLocations eventsDao;
+
+    @GetMapping("/create")
+    public String showCreateLocationForm(Model m){
+        m.addAttribute("location", new EventLocation());
+        return "/locations";
+    }
+
+    @PostMapping("/create")
+    public String createNewLocation(@ModelAttribute EventLocation locationCreated){
+        eventsDao.save(locationCreated);
+        return "/locations";
+    }
 
 }
