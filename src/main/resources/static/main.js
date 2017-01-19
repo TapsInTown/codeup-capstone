74c74c4192e6ed4aa620dc9fdbaa21dae4b7c988
josh <josh.fierrosga@gmail.com>
2017-01-19 11:34:19 -0600

    Added features to the profile page. A div that contains the discriptions of the location and a seperate div for specific events happening at said location.
    Also added some JS functionality so the div mentioned above are pushed to the side when the navbar opens.
    
    Added CSS accordingly, and all header and title tags now dafault to "Big John"
    
    Fixed thymeleaf syntax, so everything(partials/templates work as they should.

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -7,6 +7,8 @@
 function openNav() {
     document.getElementById("navigation").style.width = "25%";
     document.getElementById("move").style.marginLeft = "0";
+    document.getElementById("bodies").style.marginLeft = "30%";
+    document.getElementById("specific-events").style.marginLeft = "110%";
     /*document.body.style.backgroundColor = "white";*/ /*rgba(0,0,0,0.4)*/
 
 }
@@ -16,7 +18,9 @@ function closeNav() {
     document.getElementById("move").style.marginLeft = "0";
     document.getElementById("contact-info").style.height="0";
     document.getElementById("navigation").style.paddingTop="0";
-    document.body.style.backgroundColor = "white";
+    document.getElementById("bodies").style.marginLeft = "10%";
+    document.getElementById("specific-events").style.marginLeft = "75%";
+    /*document.body.style.backgroundColor = "white";*/
 }
 
 function openContact() {
