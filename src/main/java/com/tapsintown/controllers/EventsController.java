36ab85d5e274f8067d4f543bb932040864502415
Anthony Fortney <afortney86@gmail.com>
2017-01-19 14:50:53 -0600

    Completed Controllers and html forms for full CRUD capabilities.

--- src/main/java/com/tapsintown/controllers/EventsController.java
+++ src/main/java/com/tapsintown/controllers/EventsController.java
@@ -1,14 +1,12 @@
 package com.tapsintown.controllers;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import com.tapsintown.interfaces.Events;
 import com.tapsintown.models.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by anthonyfortney on 1/18/17.
@@ -21,16 +19,52 @@ public class EventsController {
     @Autowired
     private Events eventsDao;
 
+    @GetMapping
+    public String getEvents(Model m){
+        m.addAttribute("event", eventsDao.findAll());
+        return "event/events";
+    }
+
     @GetMapping("/create")
     public String showCreateEventForm(Model m){
         m.addAttribute("event", new Event());
-        return "/events";
+        return "event/addevent";
     }
 
     @PostMapping("/create")
     public String createNewEvent(@ModelAttribute Event eventCreated){
         eventsDao.save(eventCreated);
-        return "/events";
+        return "redirect:event/events";
     }
+
+    @GetMapping("/{id}")
+    public String showEventDetails(@PathVariable long id, Model m){
+        m.addAttribute("event", eventsDao.findOne(id));
+        return "event/eventdetails";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String showEditform(@PathVariable long id, Model m){
+        m.addAttribute("event", eventsDao.findOne(id));
+        return "event/eventedit";
+    }
+
+
+    @PostMapping("/{id}/edit")
+    public String updateEvent(Event existingEvent){
+        Event currentDetails = eventsDao.findOne(existingEvent.getId());
+        String newTitle = existingEvent.getTitle();
+        String newDate = existingEvent.getEventDate();
+        String newDescription = existingEvent.getDescription();
+
+        currentDetails.setTitle(newTitle);
+        currentDetails.setEventDate(newDate);
+        currentDetails.setDescription(newDescription);
+
+        eventsDao.save(currentDetails);
+        return "redirect:/events";
+
+    }
+
 
 }
