523be653c113dad3721087973f5de9c672e72f18
Anthony Fortney <afortney86@gmail.com>
2017-01-20 16:11:52 -0600

    default to user profile upon sucsessful login

--- src/main/java/com/tapsintown/SecurityConfiguration.java
+++ src/main/java/com/tapsintown/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/location") // user's home page, it can be any URL
+                .defaultSuccessUrl("/{id}") // user's home page, it can be any URL
                 .permitAll() // Anyone can go to the login page
                 .and()
                 .authorizeRequests()
