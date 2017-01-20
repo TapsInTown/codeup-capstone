ef904ec629c49b2aa9824fcbc084025596e0fd35
josh <josh.fierrosga@gmail.com>
2017-01-20 16:12:53 -0600

    Deleted "event" html and kept "events" html. Styled and hard coded some css for the time being for the "events" view/html.
    Added two images ("wood and more taps").
    
    Commented out some functionaltiy from the navbar for a temporary fix until I can find a solution for properly closing both navbar and the newsletter section at the same time.
    
     Couple of CSS changes.

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -10,14 +10,13 @@ function openNav() {
     document.getElementsByClassName("bodies")[0].style.marginLeft = "30%";
     document.getElementById("specific-events").style.marginLeft = "110%";
     document.getElementById("specific-events").style.opacity = "0";
-
     /*document.body.style.backgroundColor = "white";*/ /*rgba(0,0,0,0.4)*/
 }
 
 function closeNav() {
     document.getElementById("navigation").style.width = "0";
+    /*document.getElementById("contact-info").style.height="0";*/
     document.getElementsByClassName("move")[0].style.marginLeft = "0";
-    document.getElementById("contact-info").style.height="0";
     document.getElementById("navigation").style.paddingTop="0";
     document.getElementsByClassName("bodies")[0].style.marginLeft = "10%";
     document.getElementById("specific-events").style.marginLeft = "75%";
