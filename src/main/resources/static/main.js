e48714afd8f528dc66a4f2d4fd8cc8fcdcf081cf
josh <josh.fierrosga@gmail.com>
2017-01-23 11:09:12 -0600

    Fixing Map Canvas and Map Bar bug issue.

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -10,7 +10,7 @@ function openNav() {
     document.getElementById("specific-events").style.opacity = "0";
     document.getElementsByClassName("move")[0].style.marginLeft = "0";
     document.getElementsByClassName("bodies")[0].style.marginLeft = "30%";
-    document.getElementsByClassName("cards")[0].style.marginLeft = "60%";
+    /*document.getElementsByClassName("cards")[0].style.marginLeft = "30%";*/
     /*document.body.style.backgroundColor = "white";*/ /*rgba(0,0,0,0.4)*/
 }
 
@@ -21,7 +21,7 @@ function closeNav() {
     document.getElementById("specific-events").style.opacity = "1";
     document.getElementsByClassName("move")[0].style.marginLeft = "0";
     document.getElementsByClassName("bodies")[0].style.marginLeft = "10%";
-    document.getElementsByClassName("cards")[0].style.marginLeft = "20%";
+   /* document.getElementsByClassName("cards")[0].style.marginLeft = "20%";*/
     /*document.body.style.backgroundColor = "white";*/
 }
 
@@ -38,15 +38,42 @@ function closeContact() {
 /*Sliding Card Js*/
 
 $(document).ready(function(){
-        console.log("success");
     $(".mapBar").click(function(){
         $(".mapBar").toggleClass("openMapBar");
         $(".mapCanvas").toggle("mapCanvas");
+        $(".cards").style.paddingBottom = "0";
     });
     $(".mapBar").hover(function(){
         $(".mapBar").toggleClass("hoverMapBar");
     })
 });
+
+
+/*Map Canvas*/
+
+function doIt() {
+    "use strict";
+
+    var address = document.getElementById("address").innerText;
+    var mapOptions = {
+        // Set the zoom level
+        zoom: 18,
+
+        center: {
+            lat: 29.42375869999999,
+            lng: -98.4887784
+        }
+    };
+    var map = new google.maps.Map(document.getElementsByClassName('mapCanvas')[0], mapOptions);
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({"address": address}, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+        } else {
+            alert("Geocoding was not successful - STATUS: " + status);
+        }
+    });
+}
 
 
 
