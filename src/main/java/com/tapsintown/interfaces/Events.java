02f35f378e1d9f055532466d169eb99d23a82fbf
Anthony Fortney <afortney86@gmail.com>
2017-01-24 14:06:41 -0600

    created a method for finding event location id

--- src/main/java/com/tapsintown/interfaces/Events.java
+++ src/main/java/com/tapsintown/interfaces/Events.java
@@ -3,9 +3,11 @@ package com.tapsintown.interfaces;
 import com.tapsintown.models.Event;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by anthonyfortney on 1/17/17.
  */
 public interface Events extends CrudRepository<Event, Long> {
-
+    List<Event> findByEventLocationId(long id);
 }
