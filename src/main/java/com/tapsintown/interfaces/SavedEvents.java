f41fd6fb2cc34e6c0d5fc641904ab48abdfcb262
Anthony Fortney <afortney86@gmail.com>
2017-01-24 09:06:41 -0600

    events that users save show to there profile

--- src/main/java/com/tapsintown/interfaces/SavedEvents.java
+++ src/main/java/com/tapsintown/interfaces/SavedEvents.java
@@ -4,9 +4,11 @@ import com.tapsintown.models.Event;
 import com.tapsintown.models.SaveEvent;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by anthonyfortney on 1/20/17.
  */
 public interface SavedEvents extends CrudRepository<SaveEvent, Long>{
-
+    List<SaveEvent> findByUserId(long id);
 }
