629a324d7f3bf02e637b78fec12ff8d9e2f95d4d
josh <josh.fierrosga@gmail.com>
2017-01-27 10:39:06 -0600

    Fixed maps API. Now it works and has a marker on the location.

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -111,14 +111,20 @@ $(function() {
         if (status == google.maps.GeocoderStatus.OK) {
             console.log(results);
             map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                position: results[0].geometry.location,
+                map: map,
+                title: 'Hello World!'
+            });
         } else {
             alert("Geocoding was not successful - STATUS: " + status);
         }
     });
 
-}doIt();
+    }doIt();
 
-function slideUp() {
+
+    function slideUp() {
     $("#click").click(function() {
         console.log('run');
         $('#topOfDiv').animate({
